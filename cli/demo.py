c3cdbe2e57befcbd21dd0514cddc68bac8d5e1f3
Brian Reid <brian@jellyfish.co>
2022-05-01 20:56:03 -0400

    add baseline model to demo

--- cli/demo.py
+++ cli/demo.py
@@ -10,6 +10,8 @@ model = T5ForConditionalGeneration.from_pretrained(
     pretrained_model_name_or_path=model_dir, local_files_only=True
 )
 
+untuned_model = T5ForConditionalGeneration.from_pretrained('Salesforce/codet5-base')
+
 tokenizer = RobertaTokenizer.from_pretrained("Salesforce/codet5-base")
 
 
@@ -25,15 +27,22 @@ def question_to_sql(question):
         pad_token_id=tokenizer.pad_token_id,
         attention_mask=inputs.attention_mask,
         max_length=max_seq_length,
+        num_beams=5,
+        # num_beams=10,
     )
 
-    return tokenizer.decode(pred[0], skip_special_tokens=True)
+    untuned_pred = untuned_model.generate(
+        inputs.input_ids,
+        max_length=max_seq_length,        
+    )
+
+    return tokenizer.decode(pred[0], skip_special_tokens=True), tokenizer.decode(untuned_pred[0], skip_special_tokens=True)
 
 
 iface = gr.Interface(
     fn=question_to_sql,
     inputs=gr.inputs.Textbox(lines=1, placeholder="Ask a question..."),
-    outputs="text",
+    outputs=["text", "text"],
 )
 
 if __name__ == "__main__":
